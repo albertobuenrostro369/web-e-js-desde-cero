838a695e4dd5d26d79c4a7d1eb644e7b2391995a
Alberto Buenrostro <alberto.buenrostro.md@gmail.com>
2025-02-17 19:25:19 -0600

    tipos de datos

---
+++ introduccion-js/tipos-de-dato.js
@@ -0,0 +1,21 @@
+
+
+console.log("El tipo de dato 42 es:", typeof 42);
+console.log("El tipo de dato 'Veinticinco' es:", typeof 'Veinticinco');
+console.log("El tipo de dato -666 es:", typeof -666);
+console.log("El tipo de dato true es:", typeof true);
+console.log("El tipo de dato 0 es:", typeof 0);
+console.log("El tipo de dato '' es:", typeof '');
+console.log("El tipo de dato null es:", typeof null);
+console.log("El tipo de dato undefined es:", typeof undefined);
+console.log("El tipo de dato FALSE (sin definir) es:", typeof FALSE); // Error: FALSE no está definido
+
+// Mis propios ejemplos:
+console.log("El tipo de dato 3.1416 es:", typeof 3.1416); // Número decimal
+console.log("El tipo de dato [] es:", typeof []); // Array
+console.log("El tipo de dato {} es:", typeof {}); // Objeto
+console.log("El tipo de dato function(){} es:", typeof function(){}); // Función
+console.log("El tipo de dato NaN es:", typeof NaN); // Not-a-Number
+console.log("El tipo de dato Infinity es:", typeof Infinity); // Infinito
+console.log("El tipo de dato Symbol('id') es:", typeof Symbol('id')); // Symbol
+console.log("El tipo de dato BigInt(123456789012345678901234567890) es:", typeof BigInt(123456789012345678901234567890)); // BigInt
